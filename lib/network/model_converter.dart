d01abcc4203fa79b0783c8a3d68f3700ad2cfb65
jiho <ghrms80@gmail.com>
2023-01-19 05:05:27 +0900

    Encoding and decoding JSON

--- lib/network/model_converter.dart
+++ lib/network/model_converter.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'package:chopper/chopper.dart';
+import 'package:recipes/network/model_response.dart';
+import 'package:recipes/network/recipe_model.dart';
 
 // 1
-
 class ModelConverter implements Converter {
   // 2
   @override
@@ -17,10 +19,48 @@ class ModelConverter implements Converter {
     return encodeJson(req);
   }
 
-  Request encodeJson(Request request) {}
+  Request encodeJson(Request request) {
+    // 1
+    final contentType = request.headers[contentTypeKey];
+    // 2
+    if (contentType != null && contentType.contains(jsonHeaders)) {
+      // 3
+      return request.copyWith(body: json.encode(request.body));
+    }
+    return request;
+  }
 
-  Response decodeJson<BodyType, InnerType>(Response response) {}
+  Response<BodyType> decodeJson<BodyType, InnerType>(Response response) {
+    final contentType = response.headers[contentTypeKey];
+    var body = response.body;
+    // 1
+    if (contentType != null && contentType.contains(jsonHeaders)) {
+      body = utf8.decode(response.bodyBytes);
+    }
+    try {
+      // 2
+      final mapData = json.decode(body);
+      // 3
+      if (mapData['status'] != null) {
+        return response.copyWith<BodyType>(
+            body: Error(Exception(mapData['status'])) as BodyType);
+      }
+      // 4
+      final recipeQuery = APIRecipeQuery.fromJson(mapData);
+      // 5
+      return response.copyWith<BodyType>(
+          body: Success(recipeQuery) as BodyType);
+    } catch (e) {
+      // 6
+      chopperLogger.warning(e);
+      return response.copyWith<BodyType>(
+          body: Error(e as Exception) as BodyType);
+    }
+  }
 
   @override
-  Response<BodyType> ConvertResponse<BodyType, InnerType>(Response response) {}
+  Response<BodyType> ConvertResponse<BodyType, InnerType>(Response response) {
+    // 1
+    return decodeJson<BodyType, InnerType>(response);
+  }
 }
